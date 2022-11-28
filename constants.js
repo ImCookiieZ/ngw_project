c282eb98ee9cddac76208c81f42a4ca416a5bae9
ImCookiieZ <53428372+ImCookiieZ@users.noreply.github.com>
2022-11-28 14:14:20 +0100

    new questions added - execution time 8 sec

--- constants.js
+++ constants.js
@@ -8,12 +8,12 @@ export const STANDARD_TEXT = "black"
 export const ENDPOINT_URL_DBPEDIA = 'https://dbpedia.org/sparql'
 export const ENDPOINT_URL_WIKIDATA = 'https://query.wikidata.org/sparql'
 
-const getPropertyDataDbpedia = async (artist_name, property) => {
+const getPropertyDataDbpedia = async (resource, property) => {
     let result = {
         property: property,
         values: []
     }
-    const query = `SELECT ?object  WHERE { dbr:${artist_name} ${property} ?object . }`
+    const query = `SELECT ?object  WHERE { dbr:${resource} ${property} ?object . }`
     const client = new ParsingClient({ 
         endpointUrl: ENDPOINT_URL_DBPEDIA,  
         headers: {
@@ -48,7 +48,35 @@ const WikidataIds = {
     "Snoop_Dogg": "Q6096",
     //properties
     "award received" : "P166",
+    "country of citizenship" : "P27",
+    "country of origin" : "P495"
 
+}
+
+const getRandomSongFromArtistDbpedia = async (artist_name) => {
+    let songs = []
+    const query = `SELECT ?subject  WHERE { ?subject dbo:artist dbr:${artist_name} . }`
+        const client = new ParsingClient({ 
+            endpointUrl: ENDPOINT_URL_DBPEDIA,  
+            headers: {
+                "Accept": "application/json", 
+            "User-Agent": 'Next-Generation-Web-Group6/1.0 (stka22ac@student.ju.se) NGW-Project/1.0'
+        }  
+        })
+        const bindings = await client.query.select(query)
+        bindings.forEach(row => 
+            Object.entries(row).forEach(([_, value]) => {
+                songs.push(value.value)
+        }))
+        let random_songlink = songs[Math.floor(Math.random() * songs.length)]
+        let parts = random_songlink.split('/')
+        let link_songname = parts[parts.length - 1]
+        link_songname = link_songname.replaceAll(",", "\\,").replaceAll("&", "\\&").replaceAll("(", "\\(").replaceAll(")", "\\)").replaceAll("'", "\\'").replaceAll('"', '\\"').replaceAll(".", "\\.").replaceAll("!", "\\!")
+        let songname = await getPropertyDataDbpedia(link_songname, "dbp:name")
+        if (songname.values.length == 0) {
+            songname = await getPropertyDataDbpedia(link_songname, "foaf:name")
+        }
+        return songname.values[0]
 }
 
 const getPropertyDataWikidata = async (artist_name, property) => {
@@ -73,9 +101,29 @@ const getPropertyDataWikidata = async (artist_name, property) => {
     return result
 }
 
+const getCountyNameWikidata = async (entity_id) => {
+    let result = {
+        property: entity_id,
+        values: []
+    }
+    const query = `SELECT ?object  WHERE { wd:${entity_id} wdt:P1448 ?object . }`
+        const client = new ParsingClient({ 
+            endpointUrl: ENDPOINT_URL_WIKIDATA,  
+            headers: {
+                "Accept": "application/json", 
+            "User-Agent": 'Next-Generation-Web-Group6/1.0 (stka22ac@student.ju.se) NGW-Project/1.0'
+        }  
+        })
+        const bindings = await client.query.select(query)
+        bindings.forEach(row => 
+            Object.entries(row).forEach(([_, value]) => {
+            result.values.push(value.value)
+        })
+    )
+    return result
+}
 
-
-export const QUESTIONS = [{
+const activeQuestion = {
     question: "When was *artist* active?",
     getCorrectAnswer: async (artist_name) => {
         const activeYearStart = await getPropertyDataDbpedia(artist_name, "dbo:activeYearsStartYear")
@@ -115,7 +163,8 @@ export const QUESTIONS = [{
         return possible_answers
     }
 
-}, {
+}
+const archivementsQuestion = {
     question: "How many awards had *artist* got?",
     getCorrectAnswer: async (artist_name) => {
         const award_ids = await getPropertyDataWikidata(artist_name, "award received")
@@ -137,5 +186,50 @@ export const QUESTIONS = [{
         }
         return possible_answers
     }
-
-}]
+}
+const fromQuestion = {
+    question: "Where is *artist* from?",
+    getCorrectAnswer: async (artist_name) => {
+        let entity_link = await getPropertyDataWikidata(artist_name, "country of citizenship")
+        if (entity_link.values.length == 0) {
+            entity_link = await getPropertyDataWikidata(artist_name, "country of origin")
+        }
+        const parts = entity_link.values[0].split('/')
+        const entity_id = parts[parts.length - 1]
+        const countries = await getCountyNameWikidata(entity_id)
+        return `${countries.values[0]}`
+    },
+    getWrongAnswers: async (_, correct_answer) => {
+        let li = ["Germany", "Spain", "Mexico", "United Kingdom", "United States of America"]
+        li.splice(li.indexOf(correct_answer), 1);
+        li.splice(Math.floor(Math.random() * li.length), 1);
+        return li
+    }
+}
+const songQuestion =  {
+    question: "Which song did *artist* create?",
+    getCorrectAnswer: async (artist_name) => {
+        const songname = await getRandomSongFromArtistDbpedia(artist_name)
+        return `${songname}`
+    },
+    getWrongAnswers: async (current_artist, correct_answer) => {
+        let possible_answers = []
+        let artists_done = [current_artist]
+        for (let i = 0; i < 3;) {
+            const random_artist = getRandomArtistExceptDone(artists_done)
+            const songname = await getRandomSongFromArtistDbpedia(random_artist)
+            const str = `${songname}`
+            if (songname == undefined) {
+                console.log(random_artist)
+            }
+            if (possible_answers.includes(str) || correct_answer == str) {
+                continue
+            }
+            possible_answers.push(str)
+            artists_done.push(random_artist)
+            i++
+        }
+        return possible_answers
+    }
+}
+export const QUESTIONS = [activeQuestion, archivementsQuestion, fromQuestion, songQuestion]
--- demo.js
+++ demo.js
@@ -5,6 +5,7 @@ He Tan, 2021-10-26
 */
 
 import ParsingClient  from 'sparql-http-client/ParsingClient.js'
+import { ENDPOINT_URL_DBPEDIA } from './constants.js'
 
 const endpointUrl = 'https://dbpedia.org/sparql'
 // const query = `SELECT ?object  WHERE { dbr:Ed_Sheeran dbo:activeYearsEndYear ?object dbr:The_Beatles dbo:activeYearsEndYear ?object . }`
@@ -65,24 +66,73 @@ const endpointUrl = 'https://dbpedia.org/sparql'
 
 // console.log(getPropertyData("Katy_Perry", ))
 
-const WikidataIds = {
-    "Snoop_Dogg": "Q6096",
-    "award received" : "P166"
-}
-export const ENDPOINT_URL_WIKIDATA = 'https://query.wikidata.org/sparql'
+// const WikidataIds = {
+//     "Snoop_Dogg": "Q6096",
+//     "award received" : "P166"
+// }
+// export const ENDPOINT_URL_WIKIDATA = 'https://query.wikidata.org/sparql'
 
-const test = async (artist_name, property) => {
-    const query = `SELECT ?object  WHERE { wd:${WikidataIds[artist_name]} wdt:${WikidataIds[property]} ?object . }`
-        const client = new ParsingClient({ 
-            endpointUrl: ENDPOINT_URL_WIKIDATA,  
-            headers: {"Accept": "application/json"}  
-        })
-        const bindings = await client.query.select(query)
-        bindings.forEach(row => 
-            Object.entries(row).forEach(([_, value]) => {
-                console.log(value.value)
-            })
-        )
-}
-await test("Snoop_Dogg", "award received")
+// const test = async (artist_name, property) => {
+//     const query = `SELECT ?object  WHERE { wd:${artist_name} wdt:${property} ?object . }`
+//         const client = new ParsingClient({ 
+//             endpointUrl: ENDPOINT_URL_WIKIDATA,  
+//             headers: {"Accept": "application/json"}  
+//         })
+//         const bindings = await client.query.select(query)
+//         bindings.forEach(row => 
+//             Object.entries(row).forEach(([_, value]) => {
+//                 console.log(value.value)
+//             })
+//         )
+// }
+// await test("Q261", "P495")
 // console.log(WikidataIds["Snoop_Dogg"])
+// const getPropertyDataDbpedia = async (resource, property) => {
+//     let result = {
+//         property: property,
+//         values: []
+//     }
+//     const query = `SELECT ?object  WHERE { dbr:${resource} ${property} ?object . }`
+//     const client = new ParsingClient({ 
+//         endpointUrl: ENDPOINT_URL_DBPEDIA,  
+//         headers: {
+//             "Accept": "application/json",
+//             "User-Agent": 'Next-Generation-Web-Group6/1.0 (stka22ac@student.ju.se) NGW-Project/1.0'}  
+//     })
+//     const bindings = await client.query.select(query)
+//     bindings.forEach(row => 
+//         Object.entries(row).forEach(([_, value]) => {
+//             result.values.push(value.value)
+//         })
+//     )
+//     return result
+// }
+// const getRandomSongFromArtistDbpedia = async (artist_name) => {
+//     let result = {
+//         values: []
+//     }
+//     const query = `SELECT ?subject  WHERE { ?subject dbo:artist dbr:${artist_name} . }`
+//         const client = new ParsingClient({ 
+//             endpointUrl: ENDPOINT_URL_DBPEDIA,  
+//             headers: {
+//                 "Accept": "application/json", 
+//             "User-Agent": 'Next-Generation-Web-Group6/1.0 (stka22ac@student.ju.se) NGW-Project/1.0'
+//         }  
+//         })
+//         const bindings = await client.query.select(query)
+//         bindings.forEach(row => 
+//             Object.entries(row).forEach(([_, value]) => {
+//             result.values.push(value.value)
+//         }))
+//     let random_songlink = result.values[Math.floor(Math.random() * result.values.length)]
+//     while (random_songlink.includes("(")) {
+//         random_songlink = result.values[Math.floor(Math.random() * result.values.length)]
+//     }
+//     const parts = random_songlink.split('/')
+//     const songname = await getPropertyDataDbpedia(parts[parts.length - 1], "dbp:name")
+//     console.log(songname)
+// }
+
+// await getRandomSongFromArtistDbpedia("Linkin_Park")
+
+console.log("I'll_Be_Gone_(Linkin_Park_song)__I'll_Be_Gone_Vice_Remix__1".replace(",", "\\,").replace("&", "\\&").replace("(", "\\(").replace(")", "\\)").replaceAll("'", "\\'").replace('"', '\\"').replace(".", "\\.").replace("!", "\\!"))
